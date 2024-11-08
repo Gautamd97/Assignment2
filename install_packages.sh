d1959fb965ffa150f8b9ad2dfec81c1396192792
Gautamd97 <156482858+Gautamd97@users.noreply.github.com>
2024-11-07 18:42:02 -0800

    Adding explanations to install packages script

--- install_packages.sh
+++ install_packages.sh
@@ -1,17 +1,21 @@
 #!/bin/bash    
     # Shebang line -> indicates that the script should be run using bash shell
 
+# Check if the script is being run as Root, and exit if it is not being run as Root
 if [ "$UID" -ne 0 ]; then
   
     echo "This script must be run as root"
     exit 1
 fi
 
+#The cat command displays the content of the ins_packages file
+#The while loop reads each line separately and stores it in the variable package.
 cat ins_packages.txt | while read -r package; do
-package=($(< "ins_packages.txt"))
-
+#pacman -S installs the package and syncs it
+# --noconfirm automatically downloads it without requiring user input
+# $package is the name of the package called earlier in the while loop
 pacman -S --noconfirm "$package"
 
-done
+done #ends the while loop
 
 echo "Installation of packages completed"
