db1efe892c54358f6fddb446d642fc6c9881ec8b
Gautamd97 <156482858+Gautamd97@users.noreply.github.com>
2024-11-08 23:20:28 -0800

    Adding description to the script

--- Project2/new_user.sh
+++ Project2/new_user.sh
@@ -8,23 +8,31 @@ if [ "$UID" -ne 0 ]; then
   exit 1
 fi
 
-#The user will have to enter both -u and -p arguments for it to work, if one is missing, there will be an error
+# getopts allows us to handle different arguments that the user types in.
+#The user will have to enter both -u and -p arguments for this to work, if one is missing, there will be an error
+#This allows us to have a standard user input and create users consistently
 while getopts ":u:p:" opt; do
   case "${opt}" in
-    u)
+    u) 
       username="$OPTARG"
       ;;
+      # this is the option for username
+      # this assigns the argument entered by user to the variable
     p)
       password="$OPTARG"
       ;;
-    :)
+      # this is the option for password
+      # this assigns the argument entered by user to the variable
+    :) 
       echo "Option -$OPTARG requires an argument"
       exit 1
       ;;
+      # This is for when there are missing arguments for options
     \?)
       echo "Invalid option: -$OPTARG"
       exit 1
       ;;
+      # This is for when there are invalid options entered by users
   esac
 done
 
@@ -34,39 +42,38 @@ if [[ -z "$username" || -z "$password" ]]; then
   exit 1
 fi
 
-#This checks if the username already exists, and if it does it exits.
+#This checks if the username already exists, and if it does it exits. It notifies the user with the echo message if the username already exists
 if id "$username" &>/dev/null; then
-  echo "Error: User $username already exists."
+  echo "Error: User $username already exists." 
   exit 1
 fi
 
-
+# The home directory path for the new user
 home_dir="/home/$username"
-mkdir -p "$home_dir"
-echo "Created home directory: $home_dir"
+mkdir -p "$home_dir" # creates the home directory, the -p avoids any errors in case the directory already exists
 
 #This generates a UID and GID for the user and makes sure it doesnt match one in the system already
 uid=$(awk -F: '{if ($3 > max) max = $3} END {print max+1}' /etc/passwd)
 gid=$(awk -F: '{if ($3 > max) max = $3} END {print max+1}' /etc/group)
+#awk finds the maximum UID and GID and assigns it to the max variable, then adds 1 for the new GID and UID
 
-#This adds the user to the /etc/passwd
+#This adds the user to the /etc/passwd with the GID and UID generated earlier
 echo "$username:x:$uid:$gid:$username,,,:$home_dir:/bin/bash" >> /etc/passwd
-echo "Added $username to /etc/passwd"
 
-#This adds the group to /etc/group
+#This creates a group and adds the group to /etc/group
 echo "$username:x:$gid:" >> /etc/group
-echo "Created group and added $username to /etc/group"
+
 
 # setting the password for user
-# This
 echo "$username:$password" | chpasswd
 
 #grant sudo privelages to new user
+# The ALL=(ALL) ALL makes it so that the user has full sudo privelages
 echo "$username ALL=(ALL) ALL" >> /etc/sudoers
 
 #This step is to add ownership of the new directory to the new user
 
+#sets ownership to of the home directory to the new user.
 chown -R "$username:$username" "$home_dir"
-echo "Configured ownership for $home_dir"
 
 echo "User $username has been created with sudo privelages, password, and a group"
